582b79396de37da838328cd27a05e913493f3a8f
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2023-04-24 00:46:24 +0300

    Changed homepage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1>Family-m</h1>
+                <h3>Site will be finished soon...</h3>
+            </header>
+        </div>
+    );
 }
 
 export default App;
