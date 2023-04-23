e0d5b34c560998284b65565f54ef9e62344bbe6e
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2023-04-24 00:58:26 +0300

    Test deploy trigger

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <h1>Family-m</h1>
-                <h3>Site will be finished soon...</h3>
+                <h3>Site will be active soon...</h3>
+                <h5>Created by this4you</h5>
             </header>
         </div>
     );
