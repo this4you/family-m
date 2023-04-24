dfa07d5bd2394e63a7166848f8fc3b1cb62e0545
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2023-04-24 23:45:41 +0300

    Added api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.png';
 import './App.css';
+import {useEffect, useState} from "react";
+import {supabase} from "./infrastructure/api";
 
 function App() {
+    const [members, setMembers] = useState('');
+    useEffect( () => {
+        supabase.from("family_member").select().then(({data}) => {
+            console.log("FAMILY MEMBERS", data);
+            setMembers(data);
+        });
+    }, [])
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <h1>Family-m</h1>
-                <h3>Deploy test</h3>
+                <h3>Site will be active soon...!</h3>
+                <p>members: {JSON.stringify(members)}</p>
                 <h5>Created by this4you</h5>
             </header>
         </div>
